7e3f8520b640c1571c11eed4024ea889de752e49
Xcelerate-Digital-Systems <jack@xceleratedigitalsystems.com.au>
2026-02-21 17:32:58 +1100

    Your descriptive message here

--- components/admin/page-editor/PricingPreviewPanel.tsx
+++ components/admin/page-editor/PricingPreviewPanel.tsx
@@ -37,6 +37,9 @@ const DEFAULT_BRANDING: CompanyBranding = {
   font_heading: null,
   font_body: null,
   font_sidebar: null,
+  font_heading_weight: null,
+  font_body_weight: null,
+  font_sidebar_weight: null,
 };
 
 export default function PricingPreviewPanel({
